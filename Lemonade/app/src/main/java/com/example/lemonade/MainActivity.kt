3758dc7eb31acc5b7feb1a5afbc4dd8ed3656cf1
baura <mariya161617@yandex.ru>
2023-10-11 15:40:32 +0300

    Made base structure of the Lemonade App

--- Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,14 +3,29 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LemonadeTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LemonadeApp()
                 }
             }
         }
@@ -30,17 +45,105 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LemonadeApp() {
+    var currentStep by remember { mutableStateOf(1) }
+
+    when (currentStep) {
+        1 -> {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.lemon_tree),
+                    contentDescription = "Lemon tree",
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clickable {
+                            currentStep = 2
+                        }
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(
+                    text = "Tap the lemon tree to select a lemon",
+                    fontSize = 18.sp
+                )
+            }
+        }
+        2 -> {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.lemon_squeeze),
+                    contentDescription = "Lemon",
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clickable {
+                            currentStep = 3
+                        }
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(
+                    text = "Keep tapping the lemon to squeeze it",
+                    fontSize = 18.sp
+                )
+            }
+        }
+        3 -> {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.lemon_drink),
+                    contentDescription = "Glass of lemonade",
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clickable {
+                            currentStep = 4
+                        }
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(
+                    text = "Tap the lemonade to drink it",
+                    fontSize = 18.sp
+                )
+            }
+        }
+        else -> {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.lemon_restart),
+                    contentDescription = "Empty glass",
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clickable {
+                            currentStep = 1
+                        }
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(
+                    text = "Tap the empty glass to start again",
+                    fontSize = 18.sp
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LemonadeAppPreview() {
     LemonadeTheme {
-        Greeting("Android")
+        LemonadeApp()
     }
 }
